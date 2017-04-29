fa1b913467a1e3ed6893d8b486e58a4d87e2ef01
MACbook <sibdoo@RN-145-97-178-98.eduroam.rug.nl>
2017-04-29 13:54:25 +0200

    Randomized Room

--- src/Room.java
+++ src/Room.java
@@ -1,24 +1,29 @@
-import java.lang.reflect.Array;
 import java.util.*;
-import java.util.Collection;
+
 
 public class Room extends Inspectable{
 
 
 
-    private List<Doors> doorList = new ArrayList<Doors>();
+    private List<Door> doorList = new ArrayList<Door>();
 
-    private Doors door1= new Doors(Color.returnRandomColor());
-	private Doors door2= new Doors(Color.returnRandomColor());
+    private int numOfDoors;
 
     public Room(String s){
         super(s);
-        doorList.add(door1);
-        doorList.add(door2);
+        Random rand = new Random();
+        numOfDoors = rand.nextInt(5) + 1;
+        for(int i = 0; i < numOfDoors; i++) {
+            doorList.add(new Door(Color.returnRandomColor()));
+        }
     }
 
 	public void roomDescription() {
-		System.out.print("a " + color +" room with a " + doorList.get(doorList.indexOf(door1)).returnColor() + " door and a " +door2.returnColor() + " door." );
+		System.out.print("a " + color +" room with a ");
+        for(int i = 0; i < numOfDoors - 1; i++) {
+            System.out.print(doorList.get(i).returnColor() + " door and a ");
+        }
+        System.out.print(doorList.get(numOfDoors - 1).returnColor() + " door.");
 	}
 
 	public void doorDescription() {
